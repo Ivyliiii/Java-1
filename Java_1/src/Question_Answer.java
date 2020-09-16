08e34029ee4399af1c9b70403231d65c16eb7337
ivyli <ivyli@ivys-mbp>
2020-09-16 14:13:39 +0800

    Changes/Additions on the Q&A

--- Java_1/src/Question_Answer.java
+++ Java_1/src/Question_Answer.java
@@ -3,6 +3,23 @@ public class Question_Answer {
 	
 	public static void main(String[] args){
 		
-		System.out.println("Hello World");
+		System.out.println("Where are you from?");
+		
+		System.out.println("I am from Shenzhen, China, which is a comparatively new city near HongKong.");
+		
+		System.out.println("What do you normally like to do during your free time?");
+		
+		System.out.println("I really do all sorts of stuff. Normally I would just go on my phone for 5 hours straight."
+				+ "Sometimes I would read a bit (like 5 minutes). I also like watching tennis alot, so I would watch some games");
+		
+		System.out.println("What is your favorite food/drink?");
+		
+		System.out.println("My favorite drink is milk tea, so its kind of annoying how Southborough doesn't have a milk tea place"
+				+ "anywhere near. My favorite food at this moment would be anything that has cheese.");
+		
+		
+		System.out.println("What kind of music do you prefer?");
+		
+		
 	}
 }
