686977a83cc1ed455bb99dec1c05c723997bf91b
ivyli <ivyli@localhost>
2020-09-22 11:30:14 +0800

    Final Upload

---
+++ Java_1/src/LegalNamesTesting.java
@@ -0,0 +1,7 @@
+
+public class LegalNamesTesting {
+	public static void main(String[] args) {
+		char ch1 = 74;
+		System.out.println(ch1);
+	}
+}
--- Java_1/src/Question_Answer.java
+++ Java_1/src/Question_Answer.java
@@ -1,3 +1,4 @@
+// First Project: Questions and Answers
 
 public class Question_Answer {
 	
@@ -5,21 +6,25 @@ public class Question_Answer {
 		
 		System.out.println("Where are you from?");
 		
-		System.out.println("I am from Shenzhen, China, which is a comparatively new city near HongKong.");
+		System.out.println("I am from Shenzhen, China.");
 		
 		System.out.println("What do you normally like to do during your free time?");
 		
 		System.out.println("I really do all sorts of stuff. Normally I would just go on my phone for 5 hours straight."
-				+ "Sometimes I would read a bit (like 5 minutes). I also like watching tennis alot, so I would watch some games");
+				+ " Sometimes I would read a bit (like 5 minutes). I also like watching tennis alot, so I would watch some games");
 		
 		System.out.println("What is your favorite food/drink?");
 		
 		System.out.println("My favorite drink is milk tea, so its kind of annoying how Southborough doesn't have a milk tea place"
 				+ "anywhere near. My favorite food at this moment would be anything that has cheese.");
 		
-		
 		System.out.println("What kind of music do you prefer?");
 		
+		System.out.println("I listen mostly to chinese music. Normally, I would just put a song on repeat for a week or so until "
+				+ "I get tired of it.");
 		
+		System.out.println("What sorts of books do you like to read?");
+		
+		System.out.println("My favorite genre is probably scientific, but, recently, I am also trying to read a few non-fiction books.");
 	}
 }
